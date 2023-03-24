f76f19d379adb76c4be68d4aaebe8f79d9cf00d3
Bogdan Condurache <bogdan@robocorp.com>
2023-03-24 02:02:30 +0200

    just frontend

--- task.py
+++ task.py
@@ -1,44 +1,31 @@
-import os
-import subprocess
-from typing import Tuple
-import webview
-# from RPA.Assistant import Assistant
-# from RPA.Assistant.types import Size
+import http.server
+import logging
+import socketserver
+from RPA.Browser.Selenium import Selenium
 
-# assistant = Assistant()
+browser_lib = Selenium()
 
-def evaluate_js(username, password):
-    # TODO: check why this isn't working
-    def inner(window):
-        window.evaluate_js(
-        rf"""
-        document.getElementById("email").value = {username}
-        document.getElementById("password").value = {password}
-        """
-    )
-    
-    return inner
-
-
-def start_webview(username, password):
-    import time
-    time.sleep(7)
-    webview.create_window("Label Studio", "http://localhost:8080/")
-    webview.start(debug=True)
-
-def ask_credentials() -> Tuple[str]:
-    # assistant.add_text("Create user", Size.Large)
-    # assistant.add_text_input("username", placeholder="username", label="Username")
-    # assistant.add_password_input("password", label="Password")
-    # results = assistant.ask_user()
-    # return results["username"], results["password"]
-    return "username", "password"
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def do_POST(self):
+        if self.path == "/annotate":
+            content_length = int(self.headers.get("Content-Length", 0))
+            post_data = self.rfile.read(content_length).decode("utf-8")
+            print(post_data)
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write("Annotation received".encode("utf-8"))
+        else:
+            super().do_POST()
 
 if __name__ == "__main__":
-    username, password = ask_credentials()
-    os.environ["LABEL_STUDIO_DISABLE_SIGNUP_WITHOUT_LINK"] = "true"
-    os.environ["LABEL_STUDIO_USERNAME"] = username
-    os.environ["LABEL_STUDIO_PASSWORD"] = password
-    proc = subprocess.Popen(["label-studio", "-b"])
-    start_webview(username, password)
-    proc.terminate()
+    logging.basicConfig(level=logging.INFO)
+    port = 8000
+    handler = Handler
+    handler.extensions_map[".html"] = "text/html"
+
+    httpd = socketserver.TCPServer(("", port), handler)
+
+    logging.info(f"Serving at port {port}")
+    browser_lib.open_available_browser(f"http://localhost:{port}")
+    httpd.serve_forever()
